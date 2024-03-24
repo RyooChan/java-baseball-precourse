6e8e68a98cbfd38c17b61212655194bf09e01b8b
RyooChan <fbcks97@naver.com>
2024-03-25 03:04:19 +0900

    test: 숫자야구 계산 결과 문자열 출력 기능 테스트

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -1,11 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static model.BaseballNumberGenerator.BASEBALL_DIGIT_LENGTH;
+import static model.BaseballNumberMatcher.BALL;
+import static model.BaseballNumberMatcher.NOTHING;
+import static model.BaseballNumberMatcher.STRIKE;
+import static model.Game.CORRECT;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
@@ -39,5 +47,52 @@ class GameTest {
             .isThrownBy(() -> {
                 game.checkInputValidation(input);
             }).withMessage("[ERROR] %d 자리 숫자로 입력해야 합니다.", BASEBALL_DIGIT_LENGTH);
+    }
+
+    private static List<HintTestDto> makeHintDtoList() {
+        List<HintTestDto> hintTestDtoList = new ArrayList<>();
+
+        HintDto correct = new HintDto(BASEBALL_DIGIT_LENGTH, 0);
+        hintTestDtoList.add(new HintTestDto(correct, CORRECT));
+
+        HintDto twoStrikeOneBall = new HintDto(2, 1);
+        String twoStrikeOneBallMessage = 2 + STRIKE + " " + 1 + BALL;
+        hintTestDtoList.add(new HintTestDto(twoStrikeOneBall, twoStrikeOneBallMessage));
+
+        HintDto twoBall = new HintDto(0, 2);
+        String twoBallMessage = 2 + BALL;
+        hintTestDtoList.add(new HintTestDto(twoBall, twoBallMessage));
+
+        HintDto nothing = new HintDto(0, 0);
+        hintTestDtoList.add(new HintTestDto(nothing, NOTHING));
+
+        return hintTestDtoList;
+    }
+
+    private static class HintTestDto {
+        private final HintDto hintDto;
+        private final String message;
+
+        public HintTestDto(HintDto hintDto, String message) {
+            this.hintDto = hintDto;
+            this.message = message;
+        }
+
+        public HintDto getHintDto() {
+            return hintDto;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+    @ParameterizedTest
+    @DisplayName("getGameResponse 에 입력되는 값에 따라 원하는 메세지가 출력 되는지 여부를 검사한다.")
+    @MethodSource("makeHintDtoList")
+    void isGetGameResponse_ShouldReturnHintMessage(HintTestDto hintTestDto) {
+        String gameResponse = game.getGameResponse(hintTestDto.getHintDto());
+
+        assertThat(gameResponse).isEqualTo(hintTestDto.message);
     }
 }
